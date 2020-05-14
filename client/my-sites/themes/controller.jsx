76dc98dc85459cfe3afe82efd9642b06cf45e428
Yuliyan Slavchev <yuliyan.slavchev@gmail.com>
2020-05-14 19:07:25 +0300

    Remove params from request theme filters action call

--- client/my-sites/themes/controller.jsx
+++ client/my-sites/themes/controller.jsx
@@ -102,7 +102,7 @@ export function fetchThemeData( context, next ) {
 }
 
 export function fetchThemeFilters( context, next ) {
-	const { store, lang } = context;
+	const { store } = context;
 
 	if ( ! isEmpty( getThemeFilters( store.getState() ) ) ) {
 		debug( 'found theme filters in cache' );
@@ -116,13 +116,7 @@ export function fetchThemeFilters( context, next ) {
 		}
 	} );
 
-	const params = {};
-
-	if ( lang ) {
-		params.locale = lang;
-	}
-
-	store.dispatch( requestThemeFilters( params ) );
+	store.dispatch( requestThemeFilters() );
 }
 
 // Legacy (Atlas-based Theme Showcase v4) route redirects
