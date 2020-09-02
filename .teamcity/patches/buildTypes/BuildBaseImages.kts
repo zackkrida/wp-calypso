b6bc25ab5d43d858b844087e0facdde66e7ac511
sergio.cinos@automattic.com <sergio.cinos@automattic.com@TeamCity>
2020-09-02 07:58:17 +0000

    TeamCity change in 'Calypso' project: 'Build base images' build configuration settings were updated

--- .teamcity/patches/buildTypes/BuildBaseImages.kts
+++ .teamcity/patches/buildTypes/BuildBaseImages.kts
@@ -66,7 +66,6 @@ create(DslContext.projectId, BuildType({
         perfmon {
         }
         dockerSupport {
-            enabled = false
             loginToRegistry = on {
                 dockerRegistryId = "PROJECT_EXT_6"
             }
