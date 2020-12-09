dbd9db874c2acfed1bb91b1e9cd2e8e9fba0d5b4
fditrapani <filippo.di.trapani@a8c.com>
2020-12-09 13:51:32 -0500

    Remove deleted file

--- client/me/purchases/payment-methods/main.tsx
+++ client/me/purchases/payment-methods/main.tsx
@@ -29,7 +29,7 @@ export default function PaymentMethods(): JSX.Element {
 			<PageViewTracker path="/me/purchases/payment-methods" title="Me > Payment Methods" />
 			<MeSidebarNavigation />
 			<FormattedHeader brandFont headerText={ titles.sectionTitle } align="left" />
-			<PurchasesNavigation section={ 'payment-methods' } />
+			<PurchasesNavigation section={ 'paymentMethods' } />
 			<PaymentMethodList
 				addPaymentMethodUrl={
 					isEnabled( 'purchases/new-payment-methods' ) ? addNewPaymentMethod : addCreditCard
