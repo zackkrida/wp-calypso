6734a3bc31cb56546a6a9a533e78e1fb9583a0cc
Marco Ciampini <marco.ciampini@automattic.com>
2020-12-18 16:04:51 +0100

    WIP early return in composite checkout to avoid upsell page

--- client/my-sites/checkout/composite-checkout/hooks/use-create-payment-complete-callback.tsx
+++ client/my-sites/checkout/composite-checkout/hooks/use-create-payment-complete-callback.tsx
@@ -193,20 +193,21 @@ export default function useCreatePaymentCompleteCallback( {
 			}
 
 			debug( 'just redirecting to', url );
+			return;
 
-			if ( createUserAndSiteBeforeTransaction ) {
-				try {
-					window.localStorage.removeItem( 'shoppingCart' );
-					window.localStorage.removeItem( 'siteParams' );
-				} catch ( err ) {}
+			// if ( createUserAndSiteBeforeTransaction ) {
+			// 	try {
+			// 		window.localStorage.removeItem( 'shoppingCart' );
+			// 		window.localStorage.removeItem( 'siteParams' );
+			// 	} catch ( err ) {}
 
-				// We use window.location instead of page.redirect() so that the cookies are detected on fresh page load.
-				// Using page.redirect() will take to the log in page which we don't want.
-				window.location.href = url;
-				return;
-			}
+			// 	// We use window.location instead of page.redirect() so that the cookies are detected on fresh page load.
+			// 	// Using page.redirect() will take to the log in page which we don't want.
+			// 	window.location.href = url;
+			// 	return;
+			// }
 
-			page.redirect( url );
+			// page.redirect( url );
 		},
 		[
 			siteSlug,
